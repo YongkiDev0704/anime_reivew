99056f12413b77db22ac7103dac73fe995733de0
LeSheon <dlelfo7@gmail.com>
2025-04-09 19:42:11 -0400

    Current State

--- src/apps/web/src/pages/animeReview.tsx
+++ src/apps/web/src/pages/animeReview.tsx
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from "react-router-dom";
 // Query or Utils
 import { GET_REVIEWS_BY_ANILISTID } from "../graphql/reviewQuery";
 import { GET_REVIEW_ANIME_DATA_BY_ID } from "../graphql/anilistQuery";
+import { RelatedContentAnime } from "../utils/relatedContentList";
 import { WhatsNewAnime } from "../utils/whatsNewList";
 
 // Components
@@ -39,50 +40,74 @@ export const AnimeReview = () => {
         variables: { id: anilist_id },
         context: { clientName: "anilist" },
         fetchPolicy: 'cache-first',
-      });
-          // API Error, maybe move user to error page?
-          if (anilistError) {
-            navigate("/error");
-            return null;
-          }
+    });
+          
+      // API Error, maybe move user to error page?
+    if (anilistError) {
+        navigate("/error");
+        return null;
+    }
 
-          const { 
-              data: reviewsData, 
-              loading: reviewsLoading, 
+    const { 
+        data: reviewsData, 
+        loading: reviewsLoading, 
         error: reviewsError 
     } = useQuery(GET_REVIEWS_BY_ANILISTID, {
             variables: { anilist_id: 12345 }
-          });
+    });
 
-          // API Error, maybe move user to error page?
-          if (reviewsError) {
-            navigate("/error");
-            return null;
-          }
-          
-          
-          const { randomFour: whatsNewList, loading: whatsNewLoading, error: whatsNewError } = WhatsNewAnime();
+    // API Error, maybe move user to error page?
+    if (reviewsError) {
+        navigate("/error");
+        return null;
+    }
+            
+    if(anilistLoading || reviewsLoading) {
+        return (
+            <AnimeReviewWrapper>
+                <SkeletonReviewBanner />
+                <SkeletonReviewSynops />
+                <SkeletonReviewList />
+            </AnimeReviewWrapper>
+        )
+    }
+    
+    const animeData = anilistData.Media;
+    const reviews = reviewsData.getReviewsByAnilistId.data;
 
-          // API Error Redirect to Error page
-          if (whatsNewError) {
-            navigate("/error");
-            return null;
-          }
+    const randomGenreIndex = Math.floor(Math.random() * animeData.genres.length);
 
-          if(anilistLoading || reviewsLoading || whatsNewLoading) {
-            return (
-                <AnimeReviewWrapper>
-                    <SkeletonReviewBanner />
-                    <SkeletonReviewSynops />
-                    <SkeletonReviewList />
-                    <SkeletonAnimeList listType="Related Contents" />
-                    <SkeletonAnimeList listType="Something New" />
-                </AnimeReviewWrapper>
-            )
-          }
-          
-          const animeData = anilistData.Media;
-          const reviews = reviewsData.getReviewsByAnilistId.data;
+    console.log(animeData.genres[randomGenreIndex]);
+
+    const { randomFive: RelatedContList, loading: RelatedContLoading, error: RelatedContError } = RelatedContentAnime(animeData.genres[randomGenreIndex]);
+    
+
+    console.log(RelatedContList);
+
+     // API Error Redirect to Error page
+     if (RelatedContError) {
+        return <p>{RelatedContError.message}</p>
+      }
+
+    const { randomFour: whatsNewList, loading: whatsNewLoading, error: whatsNewError } = WhatsNewAnime();
+
+     // API Error Redirect to Error page
+     if (whatsNewError) {
+        navigate("/error");
+        return null;
+     }
+    
+    if(RelatedContLoading || whatsNewLoading) {
+        return (
+            <AnimeReviewWrapper>
+                <SkeletonReviewBanner />
+                <SkeletonReviewSynops />
+                <SkeletonReviewList />
+                <SkeletonAnimeList listType="Related Contents" />
+                <SkeletonAnimeList listType="Something New" />
+            </AnimeReviewWrapper>   
+        )
+    }        
 
     const formatAnime = (anime: any) => ({
         animeName: anime.title.english? anime.title.english : anime.title.romaji,
@@ -95,7 +120,7 @@ export const AnimeReview = () => {
             <ReviewBanner animeData={animeData} />
             <ReviewSynops animeData={animeData} />
             <UserReviewList reviews={reviews} />
-            {/* <AnimeList listType="Related Content"/> */}
+            <AnimeList listType="Related Content" data={RelatedContList.map(formatAnime)}/>
             <AnimeList listType="Something New" data={whatsNewList.map(formatAnime)}/>
         </AnimeReviewWrapper>
     );
