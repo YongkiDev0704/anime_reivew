07c30480239b2d316e5b5aad48bec6af6e6a0ff4
LeSheon <dlelfo7@gmail.com>
2025-04-09 07:18:57 -0400

    Add comments for logic

--- src/apps/web/src/utils/reviewList.ts
+++ src/apps/web/src/utils/reviewList.ts
@@ -1,4 +1,4 @@
-// hooks/useSeasonalAnime.ts
+
 import { useQuery } from "@apollo/client";
 import { GET_WHATS_NEW_ANIME } from "../graphql/anilistQuery";
 
@@ -8,6 +8,7 @@ export const useSeasonalAnime = () => {
   const currentYear = now.getFullYear();
   const month = now.getMonth() + 1;
 
+  // Find current season based on a today's date
   const getSeason = (month: number): "WINTER" | "SPRING" | "SUMMER" | "FALL" => {
     if (month <= 3) return "WINTER";
     if (month <= 6) return "SPRING";
@@ -16,6 +17,7 @@ export const useSeasonalAnime = () => {
   };
 
   const season = getSeason(month);
+  // Pick 1 page within Top 4 Best Score Page
   const page = Math.floor(Math.random() * 4) + 1;
 
   const { data, loading, error } = useQuery(GET_WHATS_NEW_ANIME, {
@@ -23,11 +25,12 @@ export const useSeasonalAnime = () => {
       season,
       seasonYear: currentYear,
       page,
-      perPage: 25,
+      perPage: 10,
     },
   });
 
   const mediaList = data?.Page?.media ?? [];
+  // Randomly pick 4 Animes for What's New from the selected page
   const randomFour = mediaList.sort(() => Math.random() - 0.5).slice(0, 4);
 
   return { animeList: randomFour, loading, error };
