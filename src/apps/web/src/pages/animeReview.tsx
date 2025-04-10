853abc92abe1a3db9db085488713134858294d18
YongkiDev0704 <yongkidev0704@gmail.com>
2025-04-09 18:51:45 -0700

    feat(web): Add different logic for related content in review page

--- src/apps/web/src/pages/animeReview.tsx
+++ src/apps/web/src/pages/animeReview.tsx
@@ -1,111 +1,121 @@
 import styled from "styled-components";
-
 import { useQuery } from "@apollo/client";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-// Query or Utils
 import { GET_REVIEWS_BY_ANILISTID } from "../graphql/reviewQuery";
-import { GET_REVIEW_ANIME_DATA_BY_ID } from "../graphql/anilistQuery";
-import { WhatsNewAnime } from "../utils/whatsNewList";
+import {
+  GET_REVIEW_ANIME_DATA_BY_ID,
+  GET_RELATED_ANIME,
+  GET_NEW_ANIME
+} from "../graphql/anilistQuery";
 
-// Components
 import { ReviewBanner } from "../components/ReviewBanner/ReviewBanner";
 import { ReviewSynops } from "../components/ReviewSynops/ReviewSynops";
 import { UserReviewList } from "../components/UserReviewList/UserReviewList";
 import { AnimeList } from "../components/AnimeList/AnimeList";
-import { SkeletonReviewBanner } from "../components/SkeletonReviewBanner/SkeletonReviewBanner";
-import { SkeletonReviewSynops } from "../components/SkeletonReviewSynops/SkeletonReviewSynops";
-import { SkeletonReviewList } from "../components/SkeletonReviewList/SkeletonReviewList";
-import { SkeletonAnimeList } from "../components/SkeletonAnimeList";
+
+type AnilistMedia = {
+  title: {
+    romaji: string;
+  };
+  coverImage: {
+    large: string;
+  };
+  averageScore: number;
+};
 
 export const AnimeReview = () => {
+  const { id } = useParams<{ id: string }>();
+  const anilist_id = Number(id);
 
-    const {id} = useParams<{id: string}>();
+  if (isNaN(anilist_id)) {
+    return <p>Invalid ID</p>;
+  }
 
-    // Parsing String into Number for API Call
-    const anilist_id = Number(id);
-    // Send user to error page maybe?
-    if (isNaN(anilist_id)) {
-        return <p>Invalid ID</p>;
-    }
+  const {
+    data: anilistData,
+    loading: anilistLoading,
+    error: anilistError,
+  } = useQuery(GET_REVIEW_ANIME_DATA_BY_ID, {
+    variables: { id: anilist_id },
+    context: { clientName: "anilist" },
+    fetchPolicy: "cache-first",
+  });
 
-    const navigate = useNavigate();
+  if (anilistLoading) return <p>Loading anime info...</p>;
+  if (anilistError) return <p>Error: {anilistError.message}</p>;
 
-    const {
-        data: anilistData,
-        loading: anilistLoading,
-        error: anilistError,
-      } = useQuery(GET_REVIEW_ANIME_DATA_BY_ID, {
-        variables: { id: anilist_id },
-        context: { clientName: "anilist" },
-        fetchPolicy: 'cache-first',
-    });
-          
-      // API Error, maybe move user to error page?
-    if (anilistError) {
-        navigate("/error");
-        return null;
-    }
-    
-    const { 
-        data: reviewsData, 
-        loading: reviewsLoading, 
-        error: reviewsError 
-    } = useQuery(GET_REVIEWS_BY_ANILISTID, {
-        variables: { anilist_id: 12345 }
-    });
-    
-    // API Error, maybe move user to error page?
-    if (reviewsError) {
-        navigate("/error");
-        return null;
-    }
+  const animeData = anilistData.Media;
 
-    const { randomFour: whatsNewList, loading: whatsNewLoading, error: whatsNewError } = WhatsNewAnime();
-    
-    // API Error Redirect to Error page
-    if (whatsNewError) {
-        navigate("/error");
-        return null;
-    }
-    
-    
-    if(anilistLoading || reviewsLoading || whatsNewLoading) {
-        return (
-            <AnimeReviewWrapper>
-                <SkeletonReviewBanner />
-                <SkeletonReviewSynops />
-                <SkeletonReviewList />
-                <SkeletonAnimeList listType="Related Contents" />
-                <SkeletonAnimeList listType="Something New" />
-            </AnimeReviewWrapper>   
-        )
-    }        
-    
-    const animeData = anilistData.Media;
-    const reviews = reviewsData.getReviewsByAnilistId.data;
-    
-    const formatAnime = (anime: any) => ({
-        animeName: anime.title.english? anime.title.english : anime.title.romaji,
-        animePhotoURL: anime.coverImage.large,
-        animeRating: anime.averageScore / 10,
-    });
-    
-    return (
-        <AnimeReviewWrapper>
-            <ReviewBanner animeData={animeData} />
-            <ReviewSynops animeData={animeData} />
-            <UserReviewList reviews={reviews} />
-            {/* <AnimeList listType="Related Content" data={RelatedContList.map(formatAnime)}/> */}
-            <AnimeList listType="Something New" data={whatsNewList.map(formatAnime)}/>
-        </AnimeReviewWrapper>
-    );
+  const {
+    data: reviewsData,
+    loading: reviewsLoading,
+    error: reviewsError,
+  } = useQuery(GET_REVIEWS_BY_ANILISTID, {
+    variables: { anilist_id },
+  });
+
+  const {
+    data: relatedData,
+    loading: relatedLoading,
+    error: relatedError,
+  } = useQuery(GET_RELATED_ANIME, {
+    variables: {
+      genres: animeData.genres,
+      excludeId: animeData.id,
+    },
+    context: { clientName: "anilist" },
+  });
+
+  const {
+    data: newAnimeData,
+    loading: newAnimeLoading,
+    error: newAnimeError,
+  } = useQuery(GET_NEW_ANIME, {
+    context: { clientName: "anilist" },
+  });
+
+  if (reviewsLoading || relatedLoading || newAnimeLoading)
+    return <p>Loading...</p>;
+
+  if (reviewsError) return <p>Error loading reviews: {reviewsError.message}</p>;
+  if (relatedError) return <p>Error loading related anime: {relatedError.message}</p>;
+  if (newAnimeError) return <p>Error loading new anime: {newAnimeError.message}</p>;
+
+  const reviews = reviewsData.getReviewsByAnilistId.data;
+  const relatedAnimes = relatedData?.Page?.media || [];
+  const newAnimes = newAnimeData?.Page?.media || [];
+
+  return (
+    <AnimeReviewWrapper>
+      <ReviewBanner animeData={animeData} />
+      <ReviewSynops animeData={animeData} />
+      <UserReviewList reviews={reviews} />
+      <AnimeList
+        listType="Related Content"
+        data={relatedAnimes.map((anime: AnilistMedia) => ({
+            animeName: anime.title.romaji,
+            animePhotoURL: anime.coverImage.large,
+            animeRating: anime.averageScore,
+        }))}
+    />
+
+    <AnimeList
+        listType="Something New"
+        data={newAnimes.map((anime: AnilistMedia) => ({
+            animeName: anime.title.romaji,
+            animePhotoURL: anime.coverImage.large,
+            animeRating: anime.averageScore,
+        }))}
+    />
+
+    </AnimeReviewWrapper>
+  );
 };
 
 const AnimeReviewWrapper = styled.section`
-display: flex;
-flex-direction: column;
-background-color: var(--main-background);
-padding-bottom: 70px;
-margin-bottom: 70px;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--main-background);
+  padding-bottom: 100px;
 `;
