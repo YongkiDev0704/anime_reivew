503e4e76bb6f2e3145882b9334276ac1679b4d56
YongkiDev0704 <yongkidev0704@gmail.com>
2025-04-17 13:32:35 -0700

    feat(api): Add mongoDB atlas connection

--- src/packages/core/src/constants.ts
+++ src/packages/core/src/constants.ts
@@ -5,7 +5,7 @@ dotenv.config({
   path: path.resolve(__dirname, "../../../../.env"),
 });
 
-export const MONGO_URI = "mongodb://127.0.0.1:27017/anireivew";
+export const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/anireivew";
 export const MONGO_USER = undefined;
 export const MONGO_PASS = undefined;
 export const MONGO_DB_NAME = "anireview_db";
