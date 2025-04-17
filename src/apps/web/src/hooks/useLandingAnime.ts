6296f6484f52fa803762139f17de01466bdd6844
YongkiDev0704 <yongkidev0704@gmail.com>
2025-04-17 16:45:45 -0700

    Fix(web): Change wrong axios in useLandingAnimes

--- src/apps/web/src/hooks/useLandingAnime.ts
+++ src/apps/web/src/hooks/useLandingAnime.ts
@@ -10,7 +10,7 @@ export const useLandingAnimes = () => {
   useEffect(() => {
     const fetchAnimes = async () => {
       try {
-        const res = await fetch("http://localhost:4000/api/landing-animes");
+        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/landing-animes`);
         if (!res.ok) throw new Error("Fetch failed");
 
         const data: Anime[] = await res.json();
