5f6a9a1c167fd2214fa34151b4b00bf9a2d55edf
YongkiDev0704 <yongkidev0704@gmail.com>
2025-04-17 16:00:23 -0700

    Fix(api): Resolve merge conflict

--- src/packages/core/src/constants.ts
+++ src/packages/core/src/constants.ts
@@ -5,11 +5,7 @@ dotenv.config({
   path: path.resolve(__dirname, "../../../../../.env"),
 });
 
-<<<<<<< HEAD
-export const MONGO_URI = process.env.MONGO_URI! || "mongodb://127.0.0.1:27017/anireivew";
-=======
-export const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/anireivew";
->>>>>>> build/local
+export const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/anireview";
 export const MONGO_USER = undefined;
 export const MONGO_PASS = undefined;
 export const MONGO_DB_NAME = "anireview_db";
